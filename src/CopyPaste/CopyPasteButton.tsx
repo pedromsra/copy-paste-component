79e3108206ec7f1de01bccbf4643779992e4331d
pedro <pedromsra@gmail.com>
2025-03-15 00:42:36 -0300

    documentation comments created

--- src/CopyPaste/CopyPasteButton.tsx
+++ src/CopyPaste/CopyPasteButton.tsx
@@ -6,6 +6,16 @@ function handleSetButtonColor(color: CSSProperties["color"] = "#000") {
   document.documentElement.style.setProperty("--button-color", color);
 }
 
+/**
+ * Simple Copy Button with a simple color change when clicked,
+ * after click the button turns disabled for 1 second
+ *
+ * @param text the text that should be copied to clip board
+ * @param defaultColor the color of the Icon when the button render, without interaction
+ * @param copiedColor the color of the Icon after the user interaction of click
+ *
+ */
+
 function CopyPasteButton({
   text,
   defaultColor = "inherit",
@@ -15,12 +25,21 @@ function CopyPasteButton({
   defaultColor?: CSSProperties["color"];
   copiedColor?: CSSProperties["color"];
 }) {
+  /**
+   * @state wasClicked only register if the button was clicked or not
+   */
   const [wasClicked, setWasClicked] = useState(false);
 
   useLayoutEffect(() => {
     handleSetButtonColor(defaultColor);
   }, []);
 
+  /**
+   * click handler, changes the button icon color, copy to clipboard the content of text and
+   * change the state of wasClicked to true, disabling the button, after 1 s the color back to
+   * default and wasClicked back to false, releasing the button
+   *
+   */
   function handleClick() {
     setWasClicked(true);
     handleSetButtonColor(copiedColor);
